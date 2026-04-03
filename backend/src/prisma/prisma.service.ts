1e176c685512a9e26cd9ab29ce5f1c47113bd9f0
Emanuel <emanuelcabral8@outlook.com>
2026-04-03 22:40:10 +0000

    add retries for onModuleInit

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config'; // 👈 Importamos ConfigService
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor(private configService: ConfigService) {
     super({
       datasources: {
@@ -13,12 +16,23 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
       },
     });
   }
-  
-  async onModuleInit() {
-    await this.$connect();
-  }
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
+  async onModuleInit() {
+    let retries = 5;
+    while (retries) {
+      try {
+        await this.$connect();
+        break;
+      } catch (e) {
+        retries--;
+        if (!retries) throw e;
+        console.log(`Reintentando conexión... (${retries} intentos restantes)`);
+        await new Promise((res) => setTimeout(res, 3000));
+      }
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
 }
