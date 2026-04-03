0a43a06e985698abea2c95d96c842af9bca6cbf1
Emanuel <emanuelcabral8@outlook.com>
2026-04-03 22:45:05 +0000

    delete onModuleInit

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,12 +1,9 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config'; // 👈 Importamos ConfigService
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
   constructor(private configService: ConfigService) {
     super({
       datasources: {
@@ -15,21 +12,6 @@ export class PrismaService
         },
       },
     });
-  }
-
-  async onModuleInit() {
-    let retries = 5;
-    while (retries) {
-      try {
-        await this.$connect();
-        break;
-      } catch (e) {
-        retries--;
-        if (!retries) throw e;
-        console.log(`Reintentando conexión... (${retries} intentos restantes)`);
-        await new Promise((res) => setTimeout(res, 3000));
-      }
-    }
   }
 
   async onModuleDestroy() {
